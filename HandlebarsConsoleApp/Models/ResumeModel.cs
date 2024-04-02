07983b05d64d995ab5e17b3f252d49a28b494c72
Christian Paul <christiandecembrana1@gmail.com>
2024-04-02 14:21:30 +0800

    first commit

---
+++ HandlebarsConsoleApp/Models/ResumeModel.cs
@@ -0,0 +1,52 @@
+﻿namespace HandlebarsConsoleApp.Models;
+
+public class ChildModel
+{
+    public string Name { get; set; }
+    public string DateOfBirth { get; set; }
+}
+
+public class WorkExperienceModel
+{
+    public string Title { get; set; }
+    public string Company { get; set; }
+    public string StartDate { get; set; }
+    public string EndDate { get; set; }
+}
+
+public class EducationModel
+{
+    public string Degree { get; set; }
+    public string Institution { get; set; }
+    public string GraduationYear { get; set; }
+}
+
+public class ResumeModel
+{
+    public string ProfileImageUri { get; set; }
+    public string PositionDesired { get; set; }
+    public string Date { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
+    public string EmailAddress { get; set; }
+    public string DateOfBirth { get; set; }
+    public string CivilStatus { get; set; }
+    public string Citizenship { get; set; }
+    public double Height { get; set; }
+    public int Weight { get; set; }
+    public string Spouse { get; set; }
+    public string SpouseOccupation { get; set; }
+    public List<ChildModel> Children { get; set; }
+    public string Father { get; set; }
+    public string FatherDateOfBirth { get; set; }
+    public string Mother { get; set; }
+    public string MotherDateOfBirth { get; set; }
+    public string[] LanguagesSpoken { get; set; }
+    public string EmergencyPersonContact { get; set; }
+    public string PersonContactPhoneNumber { get; set; }
+    public List<WorkExperienceModel> WorkExperience { get; set; }
+    public List<EducationModel> Education { get; set; }
+}
+
---
+++ HandlebarsConsoleApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using HandlebarsConsoleApp.Models;
+using HandlebarsDotNet;
+using Newtonsoft.Json;
+
+try
+{
+    // Path to the output directory
+    string outputDirectory = Path.Combine(Environment.CurrentDirectory, "output");
+    Directory.CreateDirectory(outputDirectory); // Create the output directory if it doesn't exist
+
+    // Template file path
+    string hbsFile = "retainer-agreement-template.hbs";
+    string templatePath = @"C:\Users\Admin\Documents\GitHub\HandlebarsConsoleApp\HandlebarsConsoleApp\" + hbsFile;
+
+    // Verify if the template file exists
+    if (!File.Exists(templatePath))
+    {
+        Console.WriteLine($"Template file not found at: {templatePath}");
+        return;
+    }
+
+    var templateSource = File.ReadAllText(templatePath);
+    var template = Handlebars.Compile(templateSource);
+
+    // Read contents of styles.css
+    //string stylesFilePath = @"C:\Users\Admin\Documents\GitHub\HandlebarsConsoleApp\HandlebarsConsoleApp\styles.css";
+    //string dynamicStyles = string.Empty;
+    //if (File.Exists(stylesFilePath))
+    //{
+    //    dynamicStyles = File.ReadAllText(stylesFilePath);
+    //}
+    //else
+    //{
+    //    Console.WriteLine($"Styles file not found at: {stylesFilePath}");
+    //}
+
+    // Sample data
+    string jsonData = "ORAData.json";
+    string json = File.ReadAllText(jsonData); // TODO - For TESTING ONLY
+    var data = JsonConvert.DeserializeObject<ORAModel>(json)!;
+
+    // Render the resume HTML with dynamic CSS styles
+    //var htmlContent = "<style>" + dynamicStyles + "</style>" + template(data);
+    var htmlContent = template(data);
+
+    // Output HTML to a file
+    string docType = "ORA";
+    string outputFile = Path.Combine(outputDirectory, $"{docType}-{data.Name}.html");
+    File.WriteAllText(outputFile, htmlContent);
+
+    Console.WriteLine($"File saved to: {outputFile}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
